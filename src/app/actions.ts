e909ba9f05d766c1a3b29e5415b2f78454c4fb0a
webify-io <webify.projects@gmail.com>
2024-12-05 20:05:10 +0200

    Created Email Templates with React
    - Created Email Templates with React
    - Sending Transactional Emails with Resend

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,18 +1,23 @@
 'use server';
 
-import { redirect } from 'next/navigation';
-import { revalidatePath } from 'next/cache';
 import { auth } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { Resend } from 'resend';
 // Payment Integration:
 import Stripe from 'stripe';
 
-import { Customers, Invoices, Status } from '@/db/schema';
 import { db } from '@/db';
+import { Customers, Invoices, type Status } from '@/db/schema';
 import { and, eq, isNull } from 'drizzle-orm';
 import { headers } from 'next/headers';
 
+import { WebifyInvoiceEmail } from '@/emails/invoice-created';
+
 // Create new instance of Payment Gateway [Stripe]:
 const stripe = new Stripe(String(process.env.STRIPE_API_SECRET));
+// Create instance of resend email
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function formCreateAction(formData: FormData) {
 	const { userId, orgId } = auth();
@@ -51,6 +56,13 @@ export async function formCreateAction(formData: FormData) {
 		.returning({
 			id: Invoices.id,
 		});
+
+	await resend.emails.send({
+		from: 'Webify <info@email.webify.org.za>',
+		to: [email],
+		subject: 'You Have a New Invoice',
+		react: WebifyInvoiceEmail({ invoiceId: results[0].id }),
+	});
 
 	redirect(`/invoices/${results[0].id}`);
 }
@@ -120,25 +132,28 @@ export async function deleteInvoiceAction(formData: FormData) {
 
 // Stripe
 export async function createPayment(formData: FormData) {
+	/* try { */
+	// Get headers and origin
 	const headersList = await headers();
 	const origin = headersList.get('origin');
+	if (!origin) throw new Error('Origin header is missing');
 
+	// Parse and validate ID from formData
 	const id = parseInt(formData.get('id') as string);
+	if (isNaN(id)) throw new Error('Invalid ID');
 
+	// Fetch invoice details from the database
 	const [result] = await db
-		.select({
-			status: Invoices.status,
-			amount: Invoices.amount,
-		})
+		.select({ status: Invoices.status, amount: Invoices.amount })
 		.from(Invoices)
 		.where(eq(Invoices.id, id))
 		.limit(1);
+	if (!result) throw new Error('Invoice not found');
 
-	// Create Checkout Sessions from body params.
+	// Create Checkout Sessions from body params
 	const session = await stripe.checkout.sessions.create({
 		line_items: [
 			{
-				// Provide the exact Price ID (for example, pr_1234) of the product you want to sell
 				price_data: {
 					currency: 'zar',
 					product: 'prod_RHYJMzAzY7T9Gy',
@@ -148,14 +163,18 @@ export async function createPayment(formData: FormData) {
 			},
 		],
 		mode: 'payment',
-		success_url: `${origin}/invoices/${id}/payment?status=success`,
-		cancel_url: `${origin}/invoices/${id}/payment?status=canceled`,
+		success_url: `${origin}/invoices/${id}/payment?status=success&session_id={CHECKOUT_SESSION_ID}`,
+		cancel_url: `${origin}/invoices/${id}/payment?status=canceled&session_id={CHECKOUT_SESSION_ID}`,
 	});
 
 	if (!session.url) {
 		throw new Error('Invalid Session URL');
 	}
 
+	// Redirect to the session URL
 	redirect(session.url);
-	/* return session.url; */ // Return the session URL instead of redirecting directly
+	/* } catch (error) {
+		console.error('Error creating payment session:', error);
+		throw new Error('Failed to create payment session');
+	} */
 }
--- src/app/invoices/[invoiceId]/payment/page.tsx
+++ src/app/invoices/[invoiceId]/payment/page.tsx
@@ -1,59 +1,67 @@
-import { useOptimistic } from 'react';
-
-import { Customers, Invoices } from '@/db/schema';
-
-import { Badge } from '@/components/ui/badge';
-import { cn } from '@/lib/utils';
-import Container from '@/components/Container';
-
-import { Button } from '@/components/ui/button';
-import { createPayment, updateStatusAction } from '@/app/actions';
+import { eq } from 'drizzle-orm';
 import {
 	CircleAlert,
 	CircleCheck,
 	CircleSlash,
 	CreditCard,
 } from 'lucide-react';
+import Stripe from 'stripe';
+
+import Container from '@/components/Container';
+import { Badge } from '@/components/ui/badge';
+import { Customers, Invoices } from '@/db/schema';
+import { cn } from '@/lib/utils';
+
+import { Button } from '@/components/ui/button';
+
+import { createPayment, updateStatusAction } from '@/app/actions';
 import { db } from '@/db';
-import { eq, and, isNull } from 'drizzle-orm';
 import { notFound } from 'next/navigation';
+
+// Create new instance of Payment Gateway [Stripe]:
+const stripe = new Stripe(String(process.env.STRIPE_API_SECRET));
 
 interface InvoicePageProps {
 	params: { invoiceId: string };
-	searchParams: { status: string };
+	searchParams: {
+		status: string;
+		session_id: string;
+	};
 }
 
 export default async function InvoicePage({
 	params,
 	searchParams,
 }: InvoicePageProps) {
-	// Awaiting params
-	const invoiceId = parseInt(params.invoiceId);
+	const invoiceId = Number.parseInt(params.invoiceId);
 
-	// searchParams
-	const isSuccess = searchParams.status === 'success';
+	const sessionId = searchParams.session_id;
+	const isSuccess = sessionId && searchParams.status === 'success';
 	const isCanceled = searchParams.status === 'canceled';
+	let isError = isSuccess && !sessionId;
 
-	console.log('invoiceId:', invoiceId);
-	// status: undefined - DEBUG
-	console.log('status:', searchParams.status);
 	console.log('isSuccess', isSuccess);
 	console.log('isCanceled', isCanceled);
 
-	if (isNaN(invoiceId)) {
+	if (Number.isNaN(invoiceId)) {
 		throw new Error('Invalid Invoice ID');
 	}
 
-	// Update status on the DB:
 	if (isSuccess) {
-		const formData = new FormData();
-		formData.append('id', String(invoiceId));
-		formData.append('status', 'paid');
+		const { payment_status } = await stripe.checkout.sessions.retrieve(
+			sessionId
+		);
 
-		await updateStatusAction(formData);
+		if (payment_status !== 'paid') {
+			isError = true;
+		} else {
+			const formData = new FormData();
+			formData.append('id', String(invoiceId));
+			formData.append('status', 'paid');
+			await updateStatusAction(formData);
+		}
 	}
 
-	// Get info from DB
 	const [result] = await db
 		.select({
 			id: Invoices.id,
@@ -78,10 +86,84 @@ export default async function InvoicePage({
 			name: result.name,
 		},
 	};
+	// Debugging to check values
+	//console.log('Params:', params);
+	//console.log('Search Params:', searchParams);
 
+	/* let pstate;
+	try {
+		pstate = await searchParams;
+		console.log('Status:', pstate.status);
+	} catch (error) {
+		console.error('Error:', error);
+	}
+
+	const isSuccess = pstate?.status === 'success';
+	const isCanceled = pstate?.status === 'canceled'; */
+
+	// Check searchParams
+	/* const isSuccess = searchParams.status === 'success';
+	const isCanceled = searchParams.status === 'canceled';
+ */
+	/* console.log('invoiceId:', invoiceId);
+	console.log('status:', searchParams.status); */
+	/* console.log('isSuccess', isSuccess);
+	console.log('isCanceled', isCanceled); */
+	/*
+	if (isNaN(invoiceId)) {
+		throw new Error('Invalid Invoice ID');
+	}
+
+	// Update status in the DB if success
+	if (isSuccess) {
+		await stripe.checkout.sessions.retrieve(session_id);
+		const formData = new FormData();
+		formData.append('id', String(invoiceId));
+		formData.append('status', 'paid');
+
+		await updateStatusAction(formData);
+	}
+
+	// Fetch invoice information from the DB
+	const [result] = await db
+		.select({
+			id: Invoices.id,
+			status: Invoices.status,
+			createTs: Invoices.createTs,
+			description: Invoices.description,
+			amount: Invoices.amount,
+			name: Customers.name,
+		})
+		.from(Invoices)
+		.innerJoin(Customers, eq(Invoices.customerId, Customers.id))
+		.where(eq(Invoices.id, invoiceId))
+		.limit(1);
+
+	if (!result) {
+		notFound();
+	}
+
+	const invoice = {
+		...result,
+		customer: {
+			name: result.name,
+		},
+	};
+ */
+	// Return or render the component
 	return (
 		<main className="w-full h-full my-12">
 			<Container>
+				{isError && (
+					<p className="bg-red-100 text-sm text-red-800 text-center px-3 py-2 rounded-lg mb-6">
+						Something went wrong, please try again!
+					</p>
+				)}
+				{isCanceled && (
+					<p className="bg-yellow-100 text-sm text-yellow-800 text-center px-3 py-2 rounded-lg mb-6">
+						Payment was canceled, please try again.
+					</p>
+				)}
 				<div className="grid grid-cols-2">
 					<div>
 						<div className="flex justify-between mb-8">
